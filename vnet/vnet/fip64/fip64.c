928a631057dff780a497e1e8ce523f5c38c3802b
Alex Bikfalvi <alex.bikfalvi@midokura.com>
2016-09-29 22:16:58 +0200

    FIP64: Add FIP64 node
    Change-Id: I2ce692959ee7ea359415d845bd87c3087ed9a23c
    Signed-off-by: Alex Bikfalvi <alex.bikfalvi@midokura.com>

---
+++ vnet/vnet/fip64/fip64.c
@@ -0,0 +1,50 @@
+/*
+ * fip64.c : FIP64 support
+ *
+ * Copyright (c) 2016 Midokura SARL.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "fip64.h"
+
+u64
+fip64_error_counter_get (u32 node_index, fip64_error_t fip64_error)
+{
+  vlib_main_t *vm = vlib_get_main ();
+  vlib_node_runtime_t *error_node = vlib_node_get_runtime (vm, node_index);
+  vlib_error_main_t *em = &vm->error_main;
+  vlib_error_t e = error_node->errors[fip64_error];
+  vlib_node_t *n = vlib_get_node (vm, node_index);
+  u32 ci;
+  
+  ci = vlib_error_get_code (e);
+  ASSERT (ci < n->n_errors);
+  ci += n->error_heap_index;
+  
+  return (em->counters[ci]);
+}
+
+static clib_error_t *
+show_fip64_stats_command_fn (vlib_main_t * vm, unformat_input_t * input,
+                             vlib_cli_command_t * cmd)
+{
+  vlib_cli_output (vm, "FIP64 stats\n");
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND(show_fip64_stats_command, static) = {
+  .path = "show fip64 stats",
+  .function = show_fip64_stats_command_fn,
+};
+/* *INDENT-ON* */
