dd9af56c1155ce0a184d2f91ee89da71a7214ed1
Dmitry Bufistov <dmitry@midokura.com>
2017-01-13 21:18:26 +0100

    FIP64: Fixed rpm package build
    Change-Id: I90f786c5ea27bc3a1ad7a1d91ea0df67677aad8a
    Signed-off-by: Dmitry Bufistov <dmitry@midokura.com>

--- vpp/vpp-api/test_client.c
+++ vpp/vpp-api/test_client.c
@@ -94,6 +94,15 @@ vlib_cli_output (struct vlib_main_t *vm, char *fmt, ...)
   clib_warning ("vlib_cli_output callled...");
 }
 
+u8 *
+format_ethernet_address (u8 * s, va_list * args)
+{
+  u8 *a = va_arg (*args, u8 *);
+
+  return format (s, "%02x:%02x:%02x:%02x:%02x:%02x",
+         a[0], a[1], a[2], a[3], a[4], a[5]);
+}
+
 static void
 vl_api_sw_interface_details_t_handler (vl_api_sw_interface_details_t * mp)
 {
@@ -343,6 +352,35 @@ vl_api_vnet_interface_counters_t_handler (vl_api_vnet_interface_counters_t *
     }
 }
 
+/* Format an IP4 address. */
+u8 *
+format_ip4_address (u8 * s, va_list * args)
+{
+  u8 *a = va_arg (*args, u8 *);
+  return format (s, "%d.%d.%d.%d", a[0], a[1], a[2], a[3]);
+}
+
+/* Parse an IP4 address %d.%d.%d.%d. */
+uword unformat_ip4_address (unformat_input_t * input, va_list * args)
+{
+  u8 * result = va_arg (*args, u8 *);
+  unsigned a[4];
+
+  if (! unformat (input, "%d.%d.%d.%d", &a[0], &a[1], &a[2], &a[3]))
+    return 0;
+
+  if (a[0] >= 256 || a[1] >= 256 || a[2] >= 256 || a[3] >= 256)
+    return 0;
+
+  result[0] = a[0];
+  result[1] = a[1];
+  result[2] = a[2];
+  result[3] = a[3];
+
+  return 1;
+}
+
+
 /* Format an IP4 route destination and length. */
 u8 *
 format_ip4_address_and_length (u8 * s, va_list * args)
@@ -434,6 +472,100 @@ format_ip6_address_and_length (u8 * s, va_list * args)
   ip6_address_t *a = va_arg (*args, ip6_address_t *);
   u8 l = va_arg (*args, u32);
   return format (s, "%U/%d", format_ip6_address, a, l);
+}
+
+/* Parse an IP6 address. */
+uword unformat_ip6_address (unformat_input_t * input, va_list * args)
+{
+  ip6_address_t * result = va_arg (*args, ip6_address_t *);
+  u16 hex_quads[8];
+  uword hex_quad, n_hex_quads, hex_digit, n_hex_digits;
+  uword c, n_colon, double_colon_index;
+
+  n_hex_quads = hex_quad = n_hex_digits = n_colon = 0;
+  double_colon_index = ARRAY_LEN (hex_quads);
+  while ((c = unformat_get_input (input)) != UNFORMAT_END_OF_INPUT)
+    {
+      hex_digit = 16;
+      if (c >= '0' && c <= '9')
+    hex_digit = c - '0';
+      else if (c >= 'a' && c <= 'f')
+    hex_digit = c + 10 - 'a';
+      else if (c >= 'A' && c <= 'F')
+    hex_digit = c + 10 - 'A';
+      else if (c == ':' && n_colon < 2)
+    n_colon++;
+      else
+    {
+      unformat_put_input (input);
+      break;
+    }
+
+      /* Too many hex quads. */
+      if (n_hex_quads >= ARRAY_LEN (hex_quads))
+    return 0;
+
+      if (hex_digit < 16)
+    {
+      hex_quad = (hex_quad << 4) | hex_digit;
+
+      /* Hex quad must fit in 16 bits. */
+      if (n_hex_digits >= 4)
+        return 0;
+
+      n_colon = 0;
+      n_hex_digits++;
+    }
+      
+      /* Save position of :: */
+      if (n_colon == 2)
+    {
+      /* More than one :: ? */
+      if (double_colon_index < ARRAY_LEN (hex_quads))
+        return 0;
+      double_colon_index = n_hex_quads;
+    }
+
+      if (n_colon > 0 && n_hex_digits > 0)
+    {
+      hex_quads[n_hex_quads++] = hex_quad;
+      hex_quad = 0;
+      n_hex_digits = 0;
+    }
+    }
+
+  if (n_hex_digits > 0)
+    hex_quads[n_hex_quads++] = hex_quad;
+
+  {
+    word i;
+
+    /* Expand :: to appropriate number of zero hex quads. */
+    if (double_colon_index < ARRAY_LEN (hex_quads))
+      {
+    word n_zero = ARRAY_LEN (hex_quads) - n_hex_quads;
+
+    for (i = n_hex_quads - 1; i >= (signed) double_colon_index; i--)
+      hex_quads[n_zero + i] = hex_quads[i];
+
+    for (i = 0; i < n_zero; i++)
+        {
+            ASSERT ((double_colon_index + i) < ARRAY_LEN (hex_quads));
+            hex_quads[double_colon_index + i] = 0;
+        }
+
+    n_hex_quads = ARRAY_LEN (hex_quads);
+      }
+
+    /* Too few hex quads given. */
+    if (n_hex_quads < ARRAY_LEN (hex_quads))
+      return 0;
+
+    for (i = 0; i < ARRAY_LEN (hex_quads); i++)
+      result->as_u16[i] = clib_host_to_net_u16 (hex_quads[i]);
+
+    return 1;
+  }
 }
 
 static void
